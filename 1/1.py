ea901d66a3d8028009b8b95e5045d2dc863326ce
Bob <michelbob@hotmail.com>
2019-12-03 13:40:18 +0100

    bug fix

--- 1/1.py
+++ 1/1.py
@@ -11,10 +11,10 @@ df.head()
 '''
 a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
 '''
-def getFuel(df):s
+def getFuel(df):
     mass = df.Mass
     return math.floor(mass / 3) - 2 
-
+    
 df['fuel'] = df.apply(lambda x: getFuel(x),axis=1)
 df.head()
 
