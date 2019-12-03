432dd38613cd69fded7e107640997f9cf1e1cb2e
Bob <michelbob@hotmail.com>
2019-12-03 14:43:44 +0100

    added second part

--- 1/1.py
+++ 1/1.py
@@ -14,10 +14,26 @@ a mass of 12, divide by 3 and round down to get 4, then subtract 2 to get 2.
 def getFuel(df):
     mass = df.Mass
     return math.floor(mass / 3) - 2 
-    
+
 df['fuel'] = df.apply(lambda x: getFuel(x),axis=1)
 df.head()
 
 print("Fuel required = {}".format(df.fuel.sum()))
+
+# %%
+
+def getInfinateFuel(fuel):
+    added = math.floor(fuel / 3) - 2
+    if added <= 0:
+        return 0
+    else:
+        return added + getInfinateFuel(added)
+
+df['inf_fuel'] = df.apply(lambda x: getInfinateFuel(x['Mass']),axis=1)
+df.head()
+
+
+print("infinate Fuel required = {}".format(df.inf_fuel.sum()))
+
 
 # %%
